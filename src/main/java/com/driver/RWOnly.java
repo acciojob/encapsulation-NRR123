7beb65180da81713a45bf52b2aef9e38d725e5d7
unknown <you@example.com>
2023-09-21 06:29:01 +0530

    commit

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String Name="Nayan";
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
 }
